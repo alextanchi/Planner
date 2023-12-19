4239756c9b70bc4ef52e041d712415abcbb7f7a4
alextanchi <alex.tanz@mail.ru>
2023-12-19 20:32:07 +0500

    update_2

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"Planner"
+	"log"
+)
 
+func main() {
+	srv := new(planner.Server)              // инициалиируем экзепляр сервера
+	if err := srv.Run("8000"); err != nil { //если во время работы метод ран возвращает ошибку то выводим ошибку и выходим
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
 }
