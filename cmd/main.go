81e0f32a9ad604efd41c5b2edec3398e21ad4ea2
alextanchi <alex.tanz@mail.ru>
2024-01-09 19:26:32 +0500

    update_4

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"Planner"
-	"Planner/pkg/handlers"
+	"Planner/pkg/handler"
 	"log"
 )
 
 // комментарий
 func main() {
-	handlers := new(handlers.Handler)                              //создаем экземпляр хендлер из нашего пакета pkg.handlers
+	handlers := new(handler.Handler)                               //создаем экземпляр хендлер из нашего пакета pkg.handler
 	srv := new(planner.Server)                                     // инициалиируем экзепляр сервера
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil { //если во время работы метод ран возвращает ошибку,
 		// тогда вызываем метод Fatal, который выведет ошибку и выйдет из приложения
