d16df24d70b04482459a6d85f1c1e2cb59be3c65
alextanchi <alex.tanz@mail.ru>
2024-01-09 19:23:31 +0500

    update_4

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"Planner"
+	"Planner/pkg/handlers"
 	"log"
 )
 
 // комментарий
 func main() {
-	srv := new(planner.Server)              // инициалиируем экзепляр сервера
-	if err := srv.Run("8000"); err != nil { //если во время работы метод ран возвращает ошибку,
+	handlers := new(handlers.Handler)                              //создаем экземпляр хендлер из нашего пакета pkg.handlers
+	srv := new(planner.Server)                                     // инициалиируем экзепляр сервера
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil { //если во время работы метод ран возвращает ошибку,
 		// тогда вызываем метод Fatal, который выведет ошибку и выйдет из приложения
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
--- server.go
+++ server.go
@@ -10,11 +10,12 @@ type Server struct {
 	httpServer *http.Server
 }
 
-func (s *Server) Run(port string) error { //запуск
+func (s *Server) Run(port string, handler http.Handler) error { //запуск //добавили в метод run аргумент хендлер типа интерфейса хендлер
 	s.httpServer = &http.Server{
 		Addr:           ":" + port,
-		MaxHeaderBytes: 1 << 20, //1MB
-		ReadTimeout:    10 * time.Second,
+		Handler:        handler,          //передали handler в объект http сервера
+		MaxHeaderBytes: 1 << 20,          //1MB
+		ReadTimeout:    10 * time.Second, //10 сек
 		WriteTimeout:   10 * time.Second,
 	}
 	return s.httpServer.ListenAndServe() //слушаем все входящие запросы для дальнейшей обработки
